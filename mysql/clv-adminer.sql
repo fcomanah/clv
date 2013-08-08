582cf4200cbc9a940cafceb3037b9e14dced839e
fcomanah <fco.manah@gmail.com>
2013-08-08 02:10:57 -0300

    keeping going

--- mysql/clv-adminer.sql
+++ mysql/clv-adminer.sql
@@ -30,6 +30,9 @@ INSERT INTO car (user_session_id, id_prd, qtd) VALUES (uid, pid, qty);
 END IF;
 END;;
 
+CREATE PROCEDURE `get_ctg`(IN `id` tinyint)
+SELECT * FROM ctg WHERE id_=id;;
+
 CREATE PROCEDURE `get_prd`(IN `pid` mediumint(8) unsigned)
 SELECT * FROM prd WHERE id_=pid;;
 
@@ -46,9 +49,35 @@ BEGIN
   SELECT * FROM ctg WHERE id_ > 1 ORDER by nme;
 END;;
 
+CREATE PROCEDURE `ls_ctg_flh`(IN `id` tinyint(3))
+BEGIN
+  SELECT * 
+  FROM ctg
+  WHERE id_ IN (
+    SELECT id_ctg_flh 
+    FROM mnu 
+    WHERE id_ctg_pai = id);
+END;;
+
+CREATE PROCEDURE `ls_ctg_pai`(IN `id` tinyint(3))
+BEGIN
+  SELECT * 
+  FROM ctg
+  WHERE id_ IN (
+    SELECT id_ctg_pai 
+    FROM mnu 
+    WHERE id_ctg_flh = id 
+    AND id_ctg_pai > 1);
+END;;
+
 CREATE PROCEDURE `ls_prd`()
 BEGIN
   SELECT * FROM prd ORDER by nme;
+END;;
+
+CREATE PROCEDURE `ls_prd_from_ctg`(IN `id` tinyint(3))
+BEGIN
+  SELECT * FROM prd WHERE id_ctg=id ORDER by nme;
 END;;
 
 CREATE PROCEDURE `ls_usr`()
@@ -92,7 +121,16 @@ CREATE TABLE `car` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `car` (`id_`, `qtd`, `user_session_id`, `id_prd`, `dtc`, `dtm`) VALUES
-(55,	2,	'3e52ddf6fae43eafafe8ba463b32016f',	4,	'2013-08-03 09:34:31',	'0000-00-00 00:00:00');
+(77,	1,	'1ef319e36ec6821e4ca667f181596992',	2,	'2013-08-03 23:32:59',	'0000-00-00 00:00:00'),
+(78,	1,	'2af5cb9bbd1d95a0a89234838afda154',	3,	'2013-08-04 09:45:21',	'2013-08-04 09:46:34'),
+(79,	7,	'2af5cb9bbd1d95a0a89234838afda154',	2,	'2013-08-04 09:46:23',	'0000-00-00 00:00:00'),
+(97,	1,	'a3abf5d946e07c70962acf58e14f8b4d',	4,	'2013-08-05 16:01:38',	'0000-00-00 00:00:00'),
+(109,	19,	'd74a4e7eb8aa675b5d45d775946e1351',	5,	'2013-08-06 10:00:42',	'0000-00-00 00:00:00'),
+(110,	1,	'd74a4e7eb8aa675b5d45d775946e1351',	4,	'2013-08-06 10:02:17',	'0000-00-00 00:00:00'),
+(111,	1,	'd9888cf173fa55b97c1c435182c174ac',	5,	'2013-08-06 20:07:31',	'0000-00-00 00:00:00'),
+(112,	3,	'd9888cf173fa55b97c1c435182c174ac',	2,	'2013-08-06 20:07:48',	'0000-00-00 00:00:00'),
+(115,	46,	'4e252102f9519bbe4e691ff3489926ad',	10,	'2013-08-07 20:51:16',	'0000-00-00 00:00:00'),
+(116,	1,	'3e52ddf6fae43eafafe8ba463b32016f',	10,	'2013-08-08 00:46:48',	'0000-00-00 00:00:00');
 
 DROP TABLE IF EXISTS `crc`;
 CREATE TABLE `crc` (
@@ -115,9 +153,20 @@ CREATE TABLE `ctg` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `ctg` (`id_`, `nme`) VALUES
+(11,	'1/s'),
+(14,	'Adesivos'),
+(8,	'BIOS'),
+(4,	'CPU'),
+(7,	'DISCO RÍGIDO'),
+(9,	'FREQUÊNCIA'),
 (2,	'Hardware'),
+(12,	'hertz'),
+(10,	'MHz'),
+(6,	'MOTHERBOARD'),
 (1,	'raiz'),
-(3,	'Software');
+(5,	'RAM'),
+(3,	'Software'),
+(13,	'Travel');
 
 DROP TABLE IF EXISTS `dsc`;
 CREATE TABLE `dsc` (
@@ -145,6 +194,20 @@ CREATE TABLE `mnu` (
   CONSTRAINT `mnu_ibfk_2` FOREIGN KEY (`id_ctg_pai`) REFERENCES `ctg` (`id_`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+INSERT INTO `mnu` (`id_ctg_flh`, `id_ctg_pai`) VALUES
+(2,	1),
+(3,	1),
+(4,	2),
+(5,	2),
+(7,	2),
+(8,	6),
+(9,	4),
+(9,	5),
+(10,	9),
+(11,	10),
+(12,	11),
+(13,	1),
+(14,	1);
 
 DROP TABLE IF EXISTS `opc`;
 CREATE TABLE `opc` (
@@ -171,11 +234,13 @@ CREATE TABLE `prd` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `prd` (`id_`, `nme`, `prc`, `dsc`, `img`, `stq`, `dtc`, `id_ctg`) VALUES
-(2,	'placa-mãe',	150.00,	'important component to mount a pc.',	'uploads/2013/06/21/mb.png',	10,	'2013-07-15 01:23:08',	1),
-(3,	'cpu',	420.00,	'brain of the machine',	'uploads/2013/06/21/cpu.png',	2,	'2013-07-15 01:24:00',	1),
-(4,	'cooler',	20.00,	'anti heating',	'uploads/2013/06/21/cooler.png',	2,	'2013-07-15 01:25:06',	1),
-(5,	'Boston City Flow',	1600.00,	'Beautiful travel!',	'uploads/2013/06/21/ram.png',	200,	'2013-07-15 01:26:07',	1),
-(6,	'disco rígido',	250.00,	'storage device',	'uploads/2013/06/21/hd.png',	100,	'2013-07-15 01:26:48',	1);
+(2,	'placa-mãe',	150.00,	'important component to mount a pc.',	'uploads/2013/06/21/mb.png',	10,	'2013-07-15 01:23:08',	6),
+(3,	'cpu',	420.00,	'brain of the machine',	'uploads/2013/06/21/cpu.png',	2,	'2013-07-15 01:24:00',	4),
+(4,	'cooler',	20.00,	'anti heating',	'uploads/2013/06/21/cooler.png',	2,	'2013-07-15 01:25:06',	2),
+(5,	'Boston City Flow Ultra big expendable Name Product example',	1599.99,	'Beautiful travel!',	'uploads/2013/06/21/ram.png',	200,	'2013-07-15 01:26:07',	13),
+(6,	'disco rígido',	250.00,	'storage device',	'uploads/2013/06/21/hd.png',	100,	'2013-07-15 01:26:48',	7),
+(9,	'Tux',	5.00,	'Adesivo Sensacional como mascote do Linux!',	'uploads/2013/08/07/Tux.png',	100,	'2013-08-07 13:48:35',	14),
+(10,	'Android Evolution',	10.00,	'Evolução das versões do Android.',	'uploads/2013/08/07/evolution-android.jpg',	50,	'2013-08-07 14:05:16',	14);
 
 DROP TABLE IF EXISTS `usr`;
 CREATE TABLE `usr` (
@@ -200,4 +265,4 @@ INSERT INTO `usr` (`id_`, `type`, `nme`, `email`, `pass`, `first_name`, `last_na
 (75,	'member',	'gladson',	'gladson@gmail.com',	UNHEX('CE8BB372405C89CF8400B914A5D455ACE5FC115C901E7454091262429E78ED70'),	'Gladson',	'Recieri',	'2013-08-30',	'2013-07-30 23:41:34',	'0000-00-00 00:00:00'),
 (73,	'admin',	'mfer',	'mfer@dcc.com',	UNHEX('CE8BB372405C89CF8400B914A5D455ACE5FC115C901E7454091262429E78ED70'),	'Manassés',	'Ferreira Neto',	'2013-08-30',	'2013-07-30 23:18:04',	'0000-00-00 00:00:00');
 
--- 2013-08-03 10:21:21
+-- 2013-08-08 02:10:16
