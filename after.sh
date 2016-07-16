7721a4b922a78677b2e0dcd24079501aadbd2702
fcomanah <fco.manah@gmail.com>
2016-07-16 15:51:59 -0300

    Update after.sh

--- after.sh
+++ after.sh
@@ -52,7 +52,7 @@ sudo apt-get install -y mysql-server php-mysql
 #sudo /usr/bin/mysql_secure_installation
 echo '--------------------------------------------------------------------'
 echo "Installing PHP"
-sudo apt-get install -y php php-mcrypt
+sudo apt-get install -y php php-mcrypt php5-curl
 
 echo '--------------------------------------------------------------------'
 echo "Clonning clv"
